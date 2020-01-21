110eb11a0f3ae620448620fc35185a40088a662e
“Patrick <pctennislessons@gmail.com>
2020-01-21 16:45:25 -0700

    updated logo and saved changes

--- meadowlark.js
+++ meadowlark.js
@@ -1,3 +1,10 @@
+const fortunes = [
+    "Conquer your fears or they will conquer you.", 
+    "Rivers need springs.", 
+    "Do not fear what you don't know.",  
+    "You will have a pleasant surprise.",  
+    "Whenever possible, keep it simple."
+]
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
 
@@ -10,11 +17,16 @@ app.set('view engine', 'handlebars')
 
 const port = process.env.port || 3000
 
+app.use(express.static(__dirname + '/public'))
+
 // index
 app.get('/', (req, res) => res.render('home'))
 
 // about route
-app.get('/about', (req, res) => res.render('about'))
+app.get('/about', (req, res) => {
+    const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
+    res.render('about', { fortune: randomFortune})
+})
 
 // custom 404 page
 app.use((req, res) => {
