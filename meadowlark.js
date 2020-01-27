619e692e4c5c86ffd4a256a4e73d76d0873d75b1
“Patrick <pctennislessons@gmail.com>
2020-01-27 16:53:41 -0700

    about and home tests done

--- meadowlark.js
+++ meadowlark.js
@@ -1,5 +1,4 @@
-
-const fortune = require('./lib/fortune')
+const handlers = require('./lib/handlers')
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
 
@@ -15,24 +14,15 @@ const port = process.env.port || 3000
 app.use(express.static(__dirname + '/public'))
 
 // index
-app.get('/', (req, res) => res.render('home'))
+app.get('/', handlers.home)
 
 // about route
-app.get('/about', (req, res) => {
-    res.render('about', { fortune: fortune.getFortune()})
-})
+app.get('/about', handlers.about)
 
 // custom 404 page
-app.use((req, res) => {
-    res.status(400)
-    res.render('404')
-})
+app.use(handlers.notFound)
 // custom 500 page
-app.use((err, req, res, next) => {
-    console.error(err.message)
-    res.status(500)
-    res.render('500')
-})
+app.use(handlers.serverError)
 
 app.listen(port, () => console.log(
     `Express started on http://localhost:${port}; ` + 
